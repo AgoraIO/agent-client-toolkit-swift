0d590d0ee61d35c3478e3250c4e747413f1a0058
Ren Jingui <renjingui@agora.io>
2026-09-15 09:50:28 +0000

    2.10.1

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AgoraAgentClientToolkit",
-            url: "https://download.agora.io/swiftpm/agent-client-toolkit-swift/2.10.0/AgoraAgentClientToolkit.xcframework.zip",
-            checksum: "291588f80c2eaabf14f1264ada9044cd84039566955844cd1885fb50c31ab641"
+            url: "https://download.agora.io/swiftpm/agent-client-toolkit-swift/2.10.1/AgoraAgentClientToolkit.xcframework.zip",
+            checksum: "a1de265c4c573ee276091db798e2844bdc8eee25abac35af9891aac88368f5a4"
         ),
         .target(
             name: "AgoraAgentClientToolkitDependencies",
